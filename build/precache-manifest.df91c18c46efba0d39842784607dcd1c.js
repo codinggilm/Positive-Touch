3a3d192c7065fe08768a90492c8157555b378ec4
coddinggilm <coddinggilm@gmail.com>
2020-11-11 08:57:24 +0000

    adding build folder

---
+++ build/precache-manifest.df91c18c46efba0d39842784607dcd1c.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05b5d51b51840a55727eb4fb5da51b32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9894d1369044f1caae25",
+    "url": "/static/css/main.990f260f.chunk.css"
+  },
+  {
+    "revision": "d0ca3a7f1385ece3f9b1",
+    "url": "/static/js/2.3515598c.chunk.js"
+  },
+  {
+    "revision": "73fcec6f3c94448eba04adf712b18ac2",
+    "url": "/static/js/2.3515598c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9894d1369044f1caae25",
+    "url": "/static/js/main.004b2205.chunk.js"
+  },
+  {
+    "revision": "3517b8c659372c5e676b",
+    "url": "/static/js/runtime-main.625876c8.js"
+  },
+  {
+    "revision": "b4f282d2074404b02558f3607330ae71",
+    "url": "/static/media/dog11.b4f282d2.jpg"
+  },
+  {
+    "revision": "e145e0f808205b962d050fc4ba109acb",
+    "url": "/static/media/dog11large.e145e0f8.jpg"
+  },
+  {
+    "revision": "ce703169f047c8914ac37fb8d2cac765",
+    "url": "/static/media/dog13.ce703169.jpg"
+  },
+  {
+    "revision": "dced36711362dd7fadd9420f770b62e1",
+    "url": "/static/media/dog16.dced3671.jpg"
+  },
+  {
+    "revision": "ed1e336aecd408f5c7830a618dc2ed18",
+    "url": "/static/media/dog17.ed1e336a.jpg"
+  },
+  {
+    "revision": "1a816266a5279f3dea95c505670b9c97",
+    "url": "/static/media/dog18.1a816266.jpg"
+  },
+  {
+    "revision": "f236885b18e6852392eed0eb3067b9a9",
+    "url": "/static/media/dog22.f236885b.jpg"
+  },
+  {
+    "revision": "289af3ceccfc9fe81fec6991c9b42ec4",
+    "url": "/static/media/dog25.289af3ce.jpg"
+  },
+  {
+    "revision": "c35f767d3e233bd5346f1e0526a906b1",
+    "url": "/static/media/dog28.c35f767d.jpg"
+  },
+  {
+    "revision": "25cf8d74805242e5d3f4836d94ee2190",
+    "url": "/static/media/dog29.25cf8d74.jpg"
+  },
+  {
+    "revision": "c469a2e17106f1fe3082325c389a0eca",
+    "url": "/static/media/dog30.c469a2e1.jpg"
+  },
+  {
+    "revision": "f361b1c55d0cd4c2722165f0ed931a3b",
+    "url": "/static/media/dog9.f361b1c5.jpg"
+  },
+  {
+    "revision": "481557a6c783f86f1a46da24b0892e4e",
+    "url": "/static/media/dog_science_white.481557a6.png"
+  },
+  {
+    "revision": "b060483e76ddefcc6adb8495fae6615d",
+    "url": "/static/media/doglarge.b060483e.jpg"
+  },
+  {
+    "revision": "4ca8def5a342ae7ccea8b15e09e2678e",
+    "url": "/static/media/dogs1.4ca8def5.jpg"
+  },
+  {
+    "revision": "aa75d9a901ec0977e09548c94c808dda",
+    "url": "/static/media/educate.aa75d9a9.jpg"
+  },
+  {
+    "revision": "84dcee8f04c3471df3ce7504d4cc0c2f",
+    "url": "/static/media/ellie1.84dcee8f.png"
+  },
+  {
+    "revision": "2a41a69e61aa6166351d5909b4b45a55",
+    "url": "/static/media/ellie10.2a41a69e.jpg"
+  },
+  {
+    "revision": "3289ac4a76a609f780cffb0a01dc3502",
+    "url": "/static/media/ellie2.3289ac4a.png"
+  },
+  {
+    "revision": "6ddd572ebd067e4ad24b4ef136d886dd",
+    "url": "/static/media/ellie3.6ddd572e.png"
+  },
+  {
+    "revision": "7c8ed0864eea878c0050f3183b3a51d2",
+    "url": "/static/media/ellie4-2.7c8ed086.png"
+  },
+  {
+    "revision": "f9196742f2f541c6f30e061520c19858",
+    "url": "/static/media/ellie4.f9196742.png"
+  },
+  {
+    "revision": "2ba8e108977db74a0c2c9ca4b0aff60e",
+    "url": "/static/media/ellie5.2ba8e108.png"
+  },
+  {
+    "revision": "a7e1e409567783b217995543e69c5445",
+    "url": "/static/media/ellie6.a7e1e409.png"
+  },
+  {
+    "revision": "ccc9320cc6fa7e727c4313e6e91534c5",
+    "url": "/static/media/ellie7.ccc9320c.png"
+  },
+  {
+    "revision": "303ecba5af0b198f8393ec5b4573fd1a",
+    "url": "/static/media/ellie8.303ecba5.jpg"
+  },
+  {
+    "revision": "b87234c09cfd5192cf57b5b155ce9b8b",
+    "url": "/static/media/ellie9.b87234c0.jpg"
+  },
+  {
+    "revision": "bb7355e26d60d90ed56544fb09ffced1",
+    "url": "/static/media/empower.bb7355e2.jpg"
+  },
+  {
+    "revision": "98f5ab9a6415d35cbd97ce718e5663ae",
+    "url": "/static/media/engage.98f5ab9a.jpg"
+  },
+  {
+    "revision": "9b750320fff3206e698bc3a87260f663",
+    "url": "/static/media/logo-dark-trans.9b750320.png"
+  },
+  {
+    "revision": "d4d00cb476f0e25b5066899f9fc6b6b1",
+    "url": "/static/media/logo-white-trans.d4d00cb4.png"
+  },
+  {
+    "revision": "c291ece503912487d2a96a3101cfd690",
+    "url": "/static/media/pillars.c291ece5.png"
+  },
+  {
+    "revision": "d1cd7974856a872e128bea73991aff31",
+    "url": "/static/media/puppy1.d1cd7974.jpg"
+  },
+  {
+    "revision": "a8e6a1e086eca111553d04df709f5769",
+    "url": "/static/media/puppy4.a8e6a1e0.jpg"
+  },
+  {
+    "revision": "8b19870b95379b60c57fbd38b1eece5a",
+    "url": "/static/media/puppy5.8b19870b.jpg"
+  },
+  {
+    "revision": "5828a36e503c354336a476bf43e2c691",
+    "url": "/static/media/vic5.5828a36e.jpg"
+  }
+]);
